19b0ba31d6fc903aa63174a855cda7e1e7a94aac
adho <adhoadhigal>
2019-02-18 11:21:18 +0300

    added an image

---
+++ README.md
@@ -0,0 +1,27 @@
+# Triangle tracker
+#### This is my triangle tracker used to show what type of a triangle it is whether its isoscles,equlateral or scalene triangle. 18th February 2019
+#### By Adho Galgallo
+## Description
+This is a triangle tracker project which enable the user to know if triangle is isoscles,equlateral or scalene triangle.
+
+
+
+## Behavior Driven  Development 
+
+## Setup/Installation Requirements
+* Open a github account.
+* Search for a project name at the repository.
+* Find project from your account and edit.
+## Known Bugs
+currently no known Bugs.
+## Technologies Used
+* HTML
+* JAVASCRIPT 
+* CSS
+## Support and contact details
+Email Address: adhoadhigal@gmail.com
+Phone Number: +254 748393309
+### License
+MIT License
+Copyright (c) 2019**Adho Galgallo**
+  
---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function Triangle(){
+
+var measurement1 = parseInt(document.getElementById("measurement1").value);
+var measurement2 = parseInt(document.getElementById("measurement2").value);
+var measurement3 = parseInt(document.getElementById("measurement3").value);
+
+var measurements = [measurement1,measurement2,measurement3];
+
+if(measurements[0] === measurements[1] && measurements[0] === measurements[2]){
+alert ("equilateral triangle!");    
+}
+else if(measurements[0]=== measurement2 || measurement2 === measurement3){
+alert ("isosceles triangle!");    
+}
+else if(measurements[0]+measurements[1]<=measurements[2] || (measurements[1]+measurement[2]<=measurements[0] || (measurements[0]+measurements[1])<=measurements[2] )){
+alert ("not a triangle!");    
+}
+else if((measurements[0]+measurements[1]) <= measurements[2] && (measurement[0] + measurements[2] <= measurements[1] || measurements[1] + measurements[2])){  
+alert ("scalene triangle!");
+}
+}
